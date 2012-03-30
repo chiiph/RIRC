2cccc446cfe65f7d2d35b8e049b5ef7f7d849772
Tomas Touceda <chiiph@torproject.org>
2012-03-30 16:13:44 -0300

    Add missing file

---
+++ src/xmlrpcauth.py
@@ -0,0 +1,45 @@
+# From http://code.activestate.com/recipes/526625-twisted-xml-rpc-server-with-basic-http-authenticat/
+
+import xmlrpclib
+from twisted.web import xmlrpc, server, http
+from twisted.internet import defer, protocol, reactor
+
+Fault = xmlrpclib.Fault
+
+class XmlRpcAuth(xmlrpc.XMLRPC):
+    def __init__(self, user='', password=''):
+        self._user = user
+        self._password = password
+        self._auth = (self._user !='')
+        xmlrpc.XMLRPC.__init__(self)
+
+    def render(self, request):
+        if self._auth:
+            cleartext_token = self._user + ':' + self._password
+            user = request.getUser()
+            passwd = request.getPassword()
+
+            if user=='' and passwd=='':
+                request.setResponseCode(http.UNAUTHORIZED)
+                return 'Authorization required!'
+            else:
+                token = user + ':' + passwd
+                if token != cleartext_token:
+                    request.setResponseCode(http.UNAUTHORIZED)
+                    return 'Authorization Failed!'
+
+        request.content.seek(0, 0)
+        args, functionPath = xmlrpclib.loads(request.content.read())
+        try:
+            function = self._getFunction(functionPath)
+        except Fault, f:
+            self._cbRender(f, request)
+        else:
+            request.setHeader("content-type", "text/xml")
+            defer.maybeDeferred(function, *args).addErrback(
+                self._ebRender
+                ).addCallback(
+                self._cbRender, request
+                )
+
+        return server.NOT_DONE_YET
